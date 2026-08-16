b909c38a30a1aa2b339a01ac1066a00b5e084662
Kareem Dwekat <149584641+kareem-Dwekat@users.noreply.github.com>
2026-08-16 20:16:30 +0300

    gghdd

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 
-import App from './App.vue'
+import App from './app.vue'
 import router from './router/index.js'
 import { createAppVuetify } from './plugins/vuetify.js'
 
